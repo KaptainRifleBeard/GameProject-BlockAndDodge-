cdb983b125fdb652ed68964e7e8d0a3ef68cdef6
KaptainRifleBeard <39362588+KaptainRifleBeard@users.noreply.github.com>
2018-05-17 13:54:14 +0800

    Block And Dodge Backup

---
+++ Run And Dodge/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public Rigidbody rb;
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// fixedUpdate is used for physics related code
+	void FixedUpdate ()
+    {
+        // Made a variable to add forwardForce 
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if ( Input.GetKey("d") )
+        {
+            rb.AddForce( sidewaysForce * Time.deltaTime, 0, 0,ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+    }
+}
